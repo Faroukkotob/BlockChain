30498a9379aafc1369840d1e7f51106a05f21dca
farouk <farouk.2002.kotob@gmail.com>
2024-04-26 16:13:00 -0400

    transaction added

--- code/hash.py
+++ code/hash.py
@@ -1,4 +1,5 @@
-import hashlib, json, sys
+import hashlib, json, sys, random
+random.seed(0)
 
 def hashMe(msg=""):
     
@@ -9,3 +10,14 @@ def hashMe(msg=""):
         return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
     else:
         return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()  #hexdigest converts the hash to a hexadecimal representation.
+
+
+def makeTransaction(maxValue=3):
+    
+    sign = int(random.getrandbits(1))*2 -1 
+    amount = random.randint(1,maxValue)
+    user1Pays = sign * amount
+    user2Pays = -1 * user1Pays
+    
+    
+    return {u'user One':user1Pays, u'User Two':user2Pays}
